82eee5002688f76464d03f6950ce89ecff644595
Mario A. Campos S <mario.campos.soberanis@gmail.com>
2018-04-10 21:41:24 -0500

    Modifications made to ask to bill cipher

--- console.js
+++ console.js
@@ -14,8 +14,8 @@ var bot = new builder.UniversalBot(connector, function(session) {
         var body = ''; // Will contain the final response
         response.on('data', function(data){ body += data; });
         response.on('end', function() {
-            var hermes_answer = JSON.parse(body);
-            session.send(hermes_answer.respuesta);
+            var parsed = JSON.parse(body);
+            session.send("Hermes:" + parsed.current_response.message);
     });
   }).on('error', function(e) {
     console.log("Got error while consulting hermes: " + e.message);
@@ -28,7 +28,7 @@ Example Request for the bot
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 function questionRequest(question) {
   var key = process.env.KEY;
-  var path = process.env.HERMES_API_PATH + '?key=' + key + '&q=' + qs.escape(question);
+  var path = process.env.HERMES_API_PATH + '?key=' + key + '&question=' + qs.escape(question) + "&session_id=mysessid";
   var options = {
     host: process.env.HERMES_HOST,
     port: '80',
